8c2c8def4ce02a2b2fe681c427936d2803726c19
sein <lsi6601@gmail.com>
2023-10-05 14:31:36 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,14 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
+  const [name, setName] = useState(null);
+  const [description, setDescription] = useState(null);
+  const [error , setError] = useState('');
+  const [data , setData] = useState([]);
+
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/v1/member').then((res)=>{
+      console.log(res.data)
+      setData(res.data);
+    }).catch((err) => console.log(err))
+
+  }, [])
+
+
+
+  const nameHandler = (e) => {
+    setName(e.target.value);
+  }
+  const descriptionHandler = (e) => {
+    setDescription(e.target.value);
+  }
+
+
+  const saveMember = () => {
+    axios.post('http://localhost:8080/api/v1/member', {
+      name : name,
+      description : description
+    })
+        .then((response) => {
+          alert("제출완료")
+        })
+
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={nameHandler}/>
+        <input onChange={descriptionHandler}/>
+        <button onClick={saveMember}>submit</button>
+
+        <div>
+          {/*{data.map((item,idx) => )}*/}
+
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
